08934d547317455517c4c959607ec826b50763b1
MarinoBal <marinobalderas984@gmail.com>
2025-09-19 22:06:19 -0600

    Descripción de los cambios

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ PATCH: actualizar una parte
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Hola mundo desde express");
+    res.send("Bienvenido ");
 
 });
 
-app.listen(3000, () => {
+app.get("/:name", (req, res, next) => {
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Bienvenido " + req.params.name);
+
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo  en puerto 3000...');
-});
+}); 
