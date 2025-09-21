d5b7844aad3da54a454855d3714098c162179703
MarinoBal <marinobalderas984@gmail.com>
2025-09-20 18:41:24 -0600

    Actualizacion a actividad 4

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const app = express();
-const {pokemon} = require('./pokedex.json');
+const { pokemon } = require('./pokedex.json');
 
-app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al pokedex");
-
+app.get("/", (req, res) => {
+  res.status(200).send("Bienvenido al pokedex");
 });
 
-app.get("/pokemon", (req, res, next) => {
-    res.status(200);
-    res.send(pokemon);
-
+app.get('/pokemon/all', (req, res) => {
+  res.status(200).send(pokemon);
 });
 
-app.get("/pokemon/:id", (req, res, next) => {
-    res.status(200);
-    res.send (pokemon[req.params.id - 1]);
+
+app.get('/pokemon/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  if (!/^\d{1,3}$/.test(req.params.id) || id < 1 || id > 151) {
+    return res.status(404).send("No se encontró el Pokémon");
+  }
+
+  res.status(200).send(pokemon[id - 1]);
 });
+
+app.get('/pokemon/name/:name', (req, res) => {
+  const name = req.params.name.toLowerCase();
+
+  const found = pokemon.find(p => p.name.toLowerCase() === name);
+
+  if (!found) {
+    return res.status(404).send("No se encontró el Pokémon");
+  }
+
+  res.status(200).send(found);
+});
+
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo  en puerto 3000...');
-}); 
+  console.log('Servidor corriendo en puerto 3000...');
+});
