2df43a4499b43e561a8352333e59e8190ddd07fe
MarinoBal <marinobalderas984@gmail.com>
2025-09-27 17:56:14 -0600

    Actualización a actividad 5

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
+const bosyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const { pokemon } = require('./pokedex.json');
 
-app.get("/", (req, res) => {
-  res.status(200).send("Bienvenido al pokedex");
+
+app.use(bosyParser.json());
+app.use(bosyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res, next) => res.status(200).send("Bienvenido al pokedex"));
+
+app.post("/pokemon", (req, res, next) => {
+  return res.status(200).send(req.body.name);
 });
 
-app.get('/pokemon/all', (req, res) => {
+
+app.get('/pokemon', (req, res, next) => {
   res.status(200).send(pokemon);
 });
 
 
-app.get('/pokemon/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
 
-  if (!/^\d{1,3}$/.test(req.params.id) || id < 1 || id > 151) {
+app.get('/pokemon/all', (req, res, next) => res.status(200).json(pokemon));
+
+
+
+app.get('/pokemon/:id', (req, res, next) => {
+  const raw = req.params.id;
+  if (!/^\d{1,3}$/.test(raw)) return next();  // <--- clave
+
+  const id = parseInt(raw, 10);
+  if (id < 1 || id > 151) {
     return res.status(404).send("No se encontró el Pokémon");
   }
-
-  res.status(200).send(pokemon[id - 1]);
+  return res.status(200).json(pokemon[id - 1]);
 });
 
-app.get('/pokemon/name/:name', (req, res) => {
-  const name = req.params.name.toLowerCase();
 
-  const found = pokemon.find(p => p.name.toLowerCase() === name);
+app.get('/pokemon/:name', (req, res, next) => {
+  const name = req.params.name;
+  const pkmn = pokemon.filter((p) => {
+    return (p.name.toLowerCase() == name.toLowerCase()) && p;
+  });
 
-  if (!found) {
-    return res.status(404).send("No se encontró el Pokémon");
+  if (pkmn.length > 0) {
+    return res.status(200).send(pkmn);
   }
 
-  res.status(200).send(found);
+  return res.status(404).send("No se encontró el Pokémon");
 });
 
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor corriendo en puerto 3000...');
+  console.log('Servidor corriendo en localhost:3000/pokemon...');
 });
