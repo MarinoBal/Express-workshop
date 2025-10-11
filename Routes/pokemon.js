965e03a2efd011313c13a6ad8f501b65d01666a4
MarinoBal <marinobalderas984@gmail.com>
2025-10-10 21:02:59 -0600

    Actualización a actividad 7

--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -11,7 +11,7 @@ pokemon.post("/", (req, res, next) => {
 pokemon.get("/", async (req, res, next) => {
    const pkmn = await db.query("SELECT * FROM pokemon");
    console.log(pkmn);
-  res.status(200).json(pkmn);
+  res.status(200).json({ code: 1, message: pkmn });
 });
 
 
@@ -21,15 +21,14 @@ pokemon.get('/all', (req, res, next) => res.status(200).json(pk));
 
 pokemon.get('/:id', (req, res, next) => {
   const raw = req.params.id;
-  if (!/^\d{1,3}$/.test(raw)) return next();  // <--- clave
+  if (!/^\d{1,3}$/.test(raw)) return next();
 
   const id = parseInt(raw, 10);
-  if (id < 1 || id > 151) {
-    return res.status(404).send("No se encontró el Pokémon");
+  if (id < 1 &&  id > 722) {
+    return res.status(404).json({ code: 0, message: "No se encontró el Pokémon" });
   }
-  return res.status(200).json(pk[id - 1]);
-}); 
-
+  return res.status(200).json({ code: 1, message: pkmn });
+});
 
 pokemon.get('/:name', (req, res, next) => {
   const name = req.params.name;
@@ -38,10 +37,10 @@ pokemon.get('/:name', (req, res, next) => {
   });
 
   if (pkmn.length > 0) {
-    return res.status(200).send(pkmn);
+    return res.status(200).json({ code: 1, message: pkmn });
   }
 
-  return res.status(404).send("No se encontró el Pokémon");
+  return res.status(404).json({ code: 0, message: "No se encontró el Pokémon" });
 });
 
 module.exports = pokemon;
--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const bosyParser = require('body-parser');
+
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const  pokemon  = require("./Routes/Pokemon");
+const  pokemon  = require('./Routes/Pokemon');
 
 app.use(morgan('dev'));
-app.use(bosyParser.json());
-app.use(bosyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use (express.urlencoded({ extended: true })); 
 
+app.get("/", (req, res, next) => res.status(200).json({ code: 1, message: "Bienvenido a la API de Pokémon" }));
 
 app.use("/pokemon", pokemon);
 
+app.use((req, res, next) => {
+  return res.status(404).json({ code: 404, message: "No se encontró la ruta" });
+});
 
-app.get("/", (req, res, next) => res.status(200).send("Bienvenido al pokedex"));
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor corriendo en localhost:3000/pokemon...');
