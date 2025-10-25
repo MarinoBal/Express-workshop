7655be925e25ad815bdae4a26f8ff79c0d721ac4
MarinoBal <marinobalderas984@gmail.com>
2025-10-25 01:15:48 -0600

    Actualizacion a actividad 9

--- Routes/pokemon.js
+++ Routes/pokemon.js
@@ -30,6 +30,97 @@ pokemon.post("/", async (req, res, next) => {
   });
 });
 
+//Borrar
+pokemon.delete("/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+  // Validar que el id sea numérico y de 1 a 3 dígitos
+  if (!/^\d{1,3}$/.test(id)) {
+    return res.status(400).json({ code: 400, message: "ID inválido" });
+  }
+
+  const query = `DELETE FROM pokemon WHERE pok_id = ${id}`;
+  const rows = await db.query(query);
+
+  if (rows.affectedRows == 1) {
+    return res.status(200).json({
+      code: 200,
+      message: "Pokémon eliminado correctamente"
+    });
+  }
+
+  return res.status(404).json({
+    code: 404,
+    message: "No se encontró el Pokémon"
+  });
+});
+
+
+//put
+pokemon.put("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+  if (!/^\d{1,3}$/.test(id)) {
+    return res.status(400).json({ code: 400, message: "ID inválido" });
+  }
+
+  if (pok_name && pok_height && pok_weight && pok_base_experience) {
+    let query = `
+      UPDATE pokemon
+      SET pok_name='${pok_name}',
+          pok_height=${pok_height},
+          pok_weight=${pok_weight},
+          pok_base_experience=${pok_base_experience}
+      WHERE pok_id=${id};
+    `;
+
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1) {
+      return res.status(200).json({
+        code: 200,
+        message: "Pokémon actualizado correctamente"
+      });
+    }
+
+    return res.status(500).json({ code: 500, message: "Error al actualizar el Pokémon" });
+  }
+
+  return res.status(400).json({
+    code: 400,
+    message: "Faltan datos obligatorios"
+  });
+});
+
+
+//patch
+pokemon.patch("/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const { pok_name } = req.body;
+
+  if (!/^\d{1,3}$/.test(id)) {
+    return res.status(400).json({ code: 400, message: "ID inválido" });
+  }
+
+  if (pok_name) {
+    const query = `UPDATE pokemon SET pok_name='${pok_name}' WHERE pok_id=${id};`;
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1) {
+      return res.status(200).json({
+        code: 200,
+        message: "Pokémon actualizado correctamente"
+      });
+    }
+
+    return res.status(500).json({ code: 500, message: "Error al actualizar el Pokémon" });
+  }
+
+  return res.status(400).json({ code: 400, message: "Faltan datos obligatorios" });
+});
+
+
 // OBTENER TODOS LOS POKÉMON
 pokemon.get("/", async (req, res, next) => {
   const pkmn = await db.query("SELECT * FROM pokemon");
